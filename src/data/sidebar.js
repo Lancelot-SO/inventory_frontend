d88015b8a89bc8fc516deae90a8294357fe926c6
Lancelot-SO <fsowah001@gmail.com>
2023-12-08 10:35:55 +0000

    initial commit

---
+++ src/data/sidebar.js
@@ -0,0 +1,38 @@
+import { FaTh, FaRegChartBar, FaCommentAlt } from "react-icons/fa";
+import { BiImageAdd } from "react-icons/bi";
+
+const menu = [
+  {
+    title: "Dashboard",
+    icon: <FaTh />,
+    path: "/dashboard",
+  },
+  {
+    title: "Add Product",
+    icon: <BiImageAdd />,
+    path: "/add-product",
+  },
+  {
+    title: "Account",
+    icon: <FaRegChartBar />,
+    childrens: [
+      {
+        title: "Profile",
+        icon1: <FaRegChartBar />,
+        path: "/profile",
+      },
+      {
+        title: "Edit Profile",
+        icon2: <FaRegChartBar />,
+        path: "/edit-profile",
+      },
+    ],
+  },
+  {
+    title: "Report Bug",
+    icon: <FaCommentAlt />,
+    path: "/contact-us",
+  },
+];
+
+export default menu;
