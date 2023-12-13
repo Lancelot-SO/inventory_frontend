1e6e876aa73b0ab87b4d3a5688cc46a0c5dc2d6f
Lancelot-SO <fsowah001@gmail.com>
2023-12-13 22:42:58 +0000

    update sidebar

--- src/data/sidebar.js
+++ src/data/sidebar.js
@@ -1,36 +1,38 @@
-import { FaTh, FaRegChartBar, FaCommentAlt } from "react-icons/fa";
-import { BiImageAdd } from "react-icons/bi";
+import { MdDashboardCustomize } from "react-icons/md";
+import { HiDocumentReport } from "react-icons/hi";
+import { IoBagAdd } from "react-icons/io5";
+import { RiAccountPinCircleFill } from "react-icons/ri";
 
 const menu = [
   {
     title: "Dashboard",
-    icon: <FaTh />,
+    icon: <MdDashboardCustomize />,
     path: "/dashboard",
   },
   {
     title: "Add Product",
-    icon: <BiImageAdd />,
+    icon: <IoBagAdd />,
     path: "/add-product",
   },
   {
     title: "Account",
-    icon: <FaRegChartBar />,
+    icon: <RiAccountPinCircleFill />,
     childrens: [
       {
         title: "Profile",
-        icon1: <FaRegChartBar />,
+        icon1: <RiAccountPinCircleFill />,
         path: "/profile",
       },
       {
         title: "Edit Profile",
-        icon2: <FaRegChartBar />,
+        icon2: <RiAccountPinCircleFill />,
         path: "/edit-profile",
       },
     ],
   },
   {
     title: "Report Bug",
-    icon: <FaCommentAlt />,
+    icon: <HiDocumentReport />,
     path: "/contact-us",
   },
 ];
